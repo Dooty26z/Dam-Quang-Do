328c5d59fa2a1e5f4bfd80fd40fb8db691cc779d
Dooty26z <23010046@st.phenikaa-uni.edu.vn>
2024-09-10 11:57:44 +0700

    Update MT2.java

--- MT2.java
+++ MT2.java
@@ -41,6 +41,8 @@ Public class DiscreteSignal implements Signal{
 
     this.buoc_song = buoc_song;
 
+    this.gtri = new HashMap<>();
+
 }
 
 public double lay_bien_do(){
@@ -72,5 +74,35 @@ public String lay_ten(){
     return ten;
 
     }
+public void them_gtri(int n, double gtri){
+    gtri.put(n,gtri);
+public double lay_gtri(double time) {
+    int n = (int)Math.round(time);
+    return gtri.getOrDefault(n,0.0);
+    }
+}
 
+public class Radar {
+    private DiscreteSignal signal;
+
+    public Radar(DiscreteSignal signal) {
+        this.signal = signal;
+    }
+     public void analyzeSignal(int n) {
+        System.out.println("Signal value at n = " + n + ": " + signal.lay_gtri(n));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            if (i <= 15) {
+                discreteSignal.them_gtri(i, 1.0);
+            } else {
+                discreteSignal.them_gtri(i, 0.0);
+            }
+        }
+        Radar radar = new Radar(discreteSignal);
+        radar.analyzeSignal(4);
+    }
 }
